d06d81d0d0b1e0ae54f9af711d228dc02ec47f66
Nick Clifford <nclifford@lcpmedia.com>
2024-10-05 20:15:10 -0500

    fish theme settings

---
+++ bobthefish.fish
@@ -0,0 +1,7 @@
+set -g theme_nerd_fonts yes
+set -g theme_color_scheme dark
+set -g theme_display_date no
+set -g theme_display_cmd_duration no
+set -g theme_display_node yes
+set -g theme_use_abbreviated_branch_name yes
+set -g theme_newline_cursor yes
